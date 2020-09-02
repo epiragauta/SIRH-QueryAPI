9c95e6e2762421d375264de74d1ddba8f3fe87b5
Edwin Piragauta <edwin.piragauta@gmail.com>
2020-09-02 09:34:37 -0500

    Primer commit del procedimiento para la consulta de datos del SIRH expuestos a través del API REST

---
+++ README.md
@@ -0,0 +1,20 @@
+# Procedimiento para la descarga de datos de SIRH (Sistema de Información del Recurso Hídrico #
+
+Fecha: Agosto de 2020
+
+Procedimiento desarrollado en Python para consultar el API REST expuesto por el Ideam para la información del SIRH.
+Los datos son descargados en formatos CSV (comma separated values).
+
+1. usuarios
+1. predios
+1. concesiones
+1. captaciones
+1. usos
+1. muestras
+1. mediciones
+1. permisosvertimiento
+1. puntosvertimiento
+1. puntosmonitoreo
+1. funias
+1. pueas
+1. fuentes
---
+++ pull_sirh_data.py
@@ -0,0 +1,72 @@
+##################################################################
+#   Descarga de datos de SIRH/api/v1/
+#    
+#   Prerequisitos: Python 2.7 o superior
+#    
+#  Este procedimiento toma la URL de los servicios REST de SIRH
+#  Realiza la autenticación para obtener el Token   
+#  
+#
+##################################################################
+import requests
+import csv
+import os
+
+url = "http://wssirh-qa.ideam.gov.co/API-SIRH/api/v1/"
+base_path = "C:/ws/MADS/SIRH/pull_data/"
+
+services = ["usuarios", "predios", "concesiones", "captaciones", "usos", "muestras", "mediciones","permisosvertimiento","puntosvertimiento","puntosmonitoreo","funias","pueas","fuentes"]
+csv_ext = ".csv"
+
+payload = "{\r\n    \"userName\":\"admin\",\r\n    \"password\":\"admin\"\r\n}\r\n"
+headers = {
+  'Content-Type': 'application/json'
+}
+
+urlAuth = url + "auth"
+response = requests.request("POST", urlAuth, headers=headers, data = payload)
+r = response.json();
+
+token =  r.get('token')
+
+urlAutoridades = url + "autoridades"
+
+payload = {}
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': token
+}
+
+response = requests.request("GET", urlAutoridades, headers=headers, data = payload)
+autoridades = response.json().get('registros')
+
+for autoridad in autoridades:
+    print ("Autoridad : " + autoridad.get('SIGLA'))
+    for s in services:
+        urlService = url + s + "/" + autoridad.get('SIGLA')
+        write_file = base_path + s + "_" + autoridad.get('SIGLA') +  csv_ext
+        if not os.path.exists(write_file):
+            print ("Executing URL : " + urlService)
+            response = requests.request("GET", urlService, headers=headers, data = payload)
+            r = response.json()
+            rows = r.get('registros')
+            
+            if len(rows) > 0:
+                title_cols = rows[0].keys()
+                
+                
+                with open(write_file,'w') as f:
+                    writer = csv.writer(f, lineterminator = '\n')
+                    writer.writerow(title_cols)
+                    for row in rows:
+                        #print(row.values())
+                        writer.writerow([unicode(s).encode("utf-8") for s in row.values()])
+                        #writer.writerow(row.values().encode('utf-8').strip())
+            else:
+                print ("No Data in service ")
+        else:
+            print("File exist dta downloaded previously")
+            
+                    
+            #print (response.text.encode('utf8'))
+
